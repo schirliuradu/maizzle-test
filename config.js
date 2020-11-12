750838ebcd6904311edd988b34ca60f5841cd156
r.schirliu <schirliuradu@gmail.com>
2020-11-12 18:53:54 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,63 @@
+/*
+|-------------------------------------------------------------------------------
+| Development config               https://maizzle.com/docs/environments/#local
+|-------------------------------------------------------------------------------
+|
+| The exported object contains the default Maizzle settings for development.
+| This is used when you run `maizzle build` or `maizzle serve` and it has
+| the fastest build time, since most transformations are disabled.
+|
+*/
+
+module.exports = {
+  build: {
+    templates: {
+      source: 'src/templates',
+      destination: {
+        path: 'build_local',
+      },
+      assets: {
+        source: 'src/assets/images',
+        destination: 'images',
+      },
+    },
+    tailwind: {
+      css: 'src/assets/css/main.css',
+    },
+  },
+
+  locals: {
+    apiData: {
+      title: "Lorem ipsum",
+      // itemsList: [
+      //   {
+      //     title: "Lorem",
+      //     text: "ipsum",
+      //     features: [
+      //       {
+      //         first: "hooray"
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     title: "Lorem",
+      //     text: "ipsum",
+      //     features: [
+      //       {
+      //         first: "hooray"
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     title: "Lorem",
+      //     text: "ipsum",
+      //     features: [
+      //       {
+      //         first: "hooray"
+      //       }
+      //     ]
+      //   },
+      // ]
+    }
+  }
+}
